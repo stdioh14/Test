a79c6020cde7296e036c6dc04d32cc124a0c9bf6
AlexB-afk <83170922+stdioh14@users.noreply.github.com>
2021-05-12 11:17:35 +0300

    mazi maine

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
+def new_mazi_func():
+    print("Poate daca mazi...")
+
+
 def mazi_func():
     mazi = 14
     print("mazi", mazi)
 
-    print("mazi milbea astazi")
+    print("mazi milbea maine")
 
 if __name__ == "__main__":
     mazi_func()
